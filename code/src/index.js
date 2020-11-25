db58a1ffddb83fa5682c4fa76ce6e73e75cf2343
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2020-11-24 23:24:02 -0300

    Back-end com Node.Js (Day 2)
    - Utilizando o Insomnia
    - Tipos de Parâmetros
    - Aplicação Funcional
    - Middlewares

--- code/src/index.js
+++ code/src/index.js
@@ -2,20 +2,42 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+function logRequest({method, url}, _, next) {
+  console.time(`[${method.toUpperCase()}] ${url}`);
+
+  next();
+
+  console.timeEnd(`[${method.toUpperCase()}] ${url}`);
+}
+
+app.use(logRequest);
+
 app.get('/resource', (request, response) => {
-  return response.json({message: 'Resource'});
+  const query = request.query;
+
+  return response.json({query, message: 'Resource'});
 });
 
 app.post('/resource', (request, response) => {
-  return response.json({message: 'Resource Created!'});
+  const body = request.body;
+
+  return response.json({body, message: 'Resource Created!'});
 });
 
 app.put('/resource/:param', (request, response) => {
-  return response.json({message: 'Resource Updated'})
+  const body = request.body;
+
+  const params = request.params;
+
+  return response.json({params, body, message: 'Resource Updated'})
 });
 
 app.delete('/resource/:param', (request, response) => {
-  return response.json({message: 'Resource Deleted'})
+  const params = request.params;
+
+  return response.json({params, message: 'Resource Deleted'})
 });
 
 app.listen(3333, () => {
